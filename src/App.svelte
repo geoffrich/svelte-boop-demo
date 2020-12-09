e62ed1d1fb805188e0b8060542d2f22b43cedbca
Geoff Rich <geoff.rich@alaskaair.com>
2020-12-09 10:24:33 -0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import HelpButton from './HelpButton.svelte';
+	import Boop from './Boop.svelte';
+	import BoopAction from './BoopAction.svelte';
+	import Expand from './Expand.svelte';
+</script>
+
+<style>
+	:global(button:focus) {
+		outline: 3px solid black;
+	}
+</style>
+
+<h1>
+	Boop hover animations
+</h1>
+
+<h2>
+	A {'<Boop>'} Component
+</h2>
+<Boop rotation={20} timing={200}>
+	<HelpButton></HelpButton>
+</Boop>
+
+<h2>
+	A <code>boop</code> action
+</h2>
+<BoopAction boopParams={{ rotation: 20, timing: 200}}>
+	<HelpButton></HelpButton>
+</BoopAction>
+<BoopAction boopParams={{ scale: 1.2, timing: 200}}>
+	<HelpButton></HelpButton>
+</BoopAction>
+<BoopAction boopParams={{ y: 5, timing: 200}}>
+	<HelpButton></HelpButton>
+</BoopAction>
+
+<h2>
+	Triggering boop on hover of whole component	
+</h2>
+<Expand></Expand>
